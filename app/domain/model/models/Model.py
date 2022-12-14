3e49119e70ca710ed094a731af3ae25279d8cd37
Alexandre <86953370+lagarrueal@users.noreply.github.com>
2022-12-15 00:57:11 +0100

    Continual improvements of the Model class

--- app/domain/model/models/Model.py
+++ app/domain/model/models/Model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+import json
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,19 +9,24 @@ from imblearn.over_sampling import SMOTE
 #FIXME: Verify the class attributes for the model
 class Model():
     name: str
-    version: str
     model : RandomForestClassifier
     model_path : str
     data_path : str
-    parameters: dict
     metrics: dict
+    metrics_path : str
+    parameters : dict
 
-    def __init__(self, name, version, model_path, data_path, parameters, metrics):
+    def __init__(self, name, version, model_path, data_path, metrics_path):
         self.name = name
-        self.version = version
+        self.model_path = model_path
         self.data_path = data_path
-        self.parameters = parameters
+        self.metrics_path = metrics_path
+        self.metrics = None
+        self.parameters = None
         self.load_model(model_path)
+        self.get_metrics()
+        self.get_parameters()
+
 
     def load_model(self, model_path):
         """ load the model from the pkl file
@@ -31,21 +36,22 @@ class Model():
         """
         self.model = pickle.load(open(model_path, "rb"))
 
-    def save_model(self, model_path: str):
+    def save_model(self, model_path: str, metrics_path : str):
         """ save the model in a pkl file 
 
         Args:
             model_path (str): path of the pkl file
         """
         pickle.dump(self.model, open(model_path, "wb"))
+        json.dump(self.metrics, open(metrics_path, "w"))
 
-    def train_model(self, data_path: str):
+    def train_model(self) :
         """ train the model with the data from the csv file
 
         Args:
             data_path (str): path to the csv file
         """
-        data = pd.read_csv(data_path)
+        data = pd.read_csv(self.data_path)
         data['quality'] = data['quality'].apply(lambda x: 'good' if x >= 7 else 'bad')
         data.drop('Id', axis=1, inplace=True)
         target = data['quality']
@@ -59,24 +65,33 @@ class Model():
                     'recall': recall_score(y_test, self.model.predict(X_test), average='weighted'),
                     'f1': f1_score(y_test, self.model.predict(X_test), average='weighted')}
         self.metrics = metrics
+        self.save_model(self.model_path, self.metrics_path)
     
-    def get_metrics(self):
+    def get_metrics(self) -> dict:
         """ returns the metrics of the model
 
         Returns:
             dict : dictionnary of the metrics, i.e accuracy, precision, recall, f1
         """
-        return self.metrics
+        if self.metrics is not None:
+            return self.metrics
+        elif self.metrics is None:
+            self.metrics = json.load(open(self.metrics_path, "r"))
+            return self.metrics
 
-    def get_parameters(self):
+    def get_parameters(self) -> dict:
         """ returns the hyperparameters of the model
 
         Returns:
             dict : dictionary of the hyperparameters
         """
-        return self.model.get_params(deep=True)
+        if self.parameters is not None:
+            return self.parameters
+        elif self.parameters is None:
+            self.parameters = self.model.get_params()
+            return self.parameters
     
-    def predict(self, wine):
+    def predict(self, wine) -> str:
         """ predict the quality of the wine
 
         Args:
