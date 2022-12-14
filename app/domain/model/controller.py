84ca73f709e1be61309a779951f1e43369f15833
Alexandre <86953370+lagarrueal@users.noreply.github.com>
2022-12-15 00:54:42 +0100

    get_attributes() and retrain() working

--- app/domain/model/controller.py
+++ app/domain/model/controller.py
@@ -1,20 +1,33 @@
 from .models.Model import Model
 
+
+
 #TODO: Generate a MEGA/Onedrive link to download the model?
-async def get_serialized_model() -> str:
-    return "url"
+async def get_serialized_model() -> Model:
+    """return the serialized model
+    
+    Returns:
+        Model: the serialized model
+    """
+    model = Model(name="RF Classifier", version="1.0", model_path="datasource/wine_quality.pkl", data_path="datasource/Wines.csv", metrics_path="datasource/model_metrics.json")
+    return model
 
-async def get_attributes() -> Model:
-    return Model(
-        name="",
-        version="",
-        path="",
-        parameters=[],
-        metrics=[],
-    )
+async def get_attributes() -> dict:
+    """return the attributes of the model, e.g. name, version, parameters, metrics
 
-async def add_whine_to_model(id: int):
+    Returns:
+        dict: dict with 3 keys: Global_info, parameters, metrics, each key contains a dict with the corresponding attributes
+    """
+    model = await get_serialized_model()
+    return { "Global_info" : {"name": model.name},
+            "parameters": model.parameters,
+            "metrics": model.metrics,}
+
+async def add_wine_to_model(id: int):
     pass
 
 async def retrain():
-    pass
+    """get the model and retrain it with the new data
+    """
+    model = await get_serialized_model()
+    model.train_model()
