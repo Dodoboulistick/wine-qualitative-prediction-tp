308470d167a9ef781c71e8811cb1aeba598cc73e
Alexandre <86953370+lagarrueal@users.noreply.github.com>
2022-12-18 13:53:56 +0100

    Updated the method to get the best wine
    We take all the good wines and return the one with the highest alcohol value.

--- app/domain/predict/controller.py
+++ app/domain/predict/controller.py
@@ -1,15 +1,42 @@
+import pandas as pd
 from domain.wine.models.Wine import Wine
 from domain.wine.controller import get_all_wines
 from domain.model.controller import get_serialized_model
 
 async def predict_wine_quality(wine : Wine) -> dict:
+    """ predict the quality of a wine given its attributes 
+
+    Args:
+        wine (Wine): the wine to predict
+
+    Returns:
+        dict: a dictionary containing the wine quality, e.g {'wine_quality': "good"}
+    """
     model = await get_serialized_model()
     wine = wine.to_list()
     quality = model.predict(wine)[0]
     return {'wine_quality': quality}
 
 async def predict_best_wine() -> Wine:
+    """return the feature of the virtually best wine
+
+    Returns:
+        Wine: a Wine object containing the best wine features
+    """
     wines = await get_all_wines()
-    ranked_wines = sorted(wines, key=lambda wine: int(wine.quality), reverse=True)
-    return ranked_wines[0]
+    wines = dict(map(lambda wine: (wine.Id, wine), wines))
+    wines = list(map(lambda wine: wine.to_list(extra = True), wines.values()))
+    wines = pd.DataFrame(wines, columns = ['fixed_acidity', 'volatile_acidity', 
+                                            'citric_acid', 'residual_sugar',
+                                            'chlorides', 'free_sulfur_dioxide', 
+                                            'total_sulfur_dioxide', 'density', 
+                                            'pH', 'sulphates', 
+                                            'alcohol', 'quality', 
+                                            'Id'])
+    wines.dropna(inplace = True)
+    wines['quality'] = wines['quality'].astype(int)
+    wines["quality"] = wines["quality"].apply(lambda x: 'good' if x >= 7 else 'bad')
+    wines = wines[wines['quality'] == 'good']
+    best_wine = wines[wines['alcohol'] == wines['alcohol'].max()]
+    return Wine(**best_wine.to_dict('records')[0])
     
--- app/domain/wine/models/Wine.py
+++ app/domain/wine/models/Wine.py
@@ -21,20 +21,38 @@ class Wine(BaseModel):
     quality: Union[str,None]
     Id: Union[int,None]
 
-    def to_list(self) -> list:
+    def to_list(self, extra = False) -> list:
         """Convert a wine object to a list
+
+        Args:
+            extra (bool, optional): if True, the list will contain the wine quality and Id. Defaults to False.
 
         Returns:
             list: a list of wine attributes
         """
-        return [self.fixed_acidity, 
-                self.volatile_acidity, 
-                self.citric_acid, 
-                self.residual_sugar, 
-                self.chlorides, 
-                self.free_sulfur_dioxide, 
-                self.total_sulfur_dioxide, 
-                self.density, 
-                self.pH, 
-                self.sulphates, 
-                self.alcohol]
+        if extra == False:
+            return [self.fixed_acidity, 
+                    self.volatile_acidity, 
+                    self.citric_acid, 
+                    self.residual_sugar, 
+                    self.chlorides, 
+                    self.free_sulfur_dioxide, 
+                    self.total_sulfur_dioxide, 
+                    self.density, 
+                    self.pH, 
+                    self.sulphates, 
+                    self.alcohol]
+        elif extra == True:
+            return [self.fixed_acidity, 
+                    self.volatile_acidity, 
+                    self.citric_acid, 
+                    self.residual_sugar, 
+                    self.chlorides, 
+                    self.free_sulfur_dioxide, 
+                    self.total_sulfur_dioxide, 
+                    self.density, 
+                    self.pH, 
+                    self.sulphates, 
+                    self.alcohol,
+                    self.quality,
+                    self.Id]
